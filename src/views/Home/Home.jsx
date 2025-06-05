2c4303200bdd7eadf155ff13a7cb2078b37b6cbf
IPYURA <0207ehdrjs@naver.com>
2025-06-05 23:56:02 +0900

    css prettier

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -2,29 +2,43 @@ import { useContext } from 'react';
 import TestNumContext from '../../Context/TestNumContext';
 
 const Home = () => {
-    const { globalVal, setGlobalVal } = useContext(TestNumContext);
+  const { globalVal, setGlobalVal } = useContext(TestNumContext);
 
-    const handleClick = () => {
-        setGlobalVal(globalVal + 1);
-    };
-    return (
-        <>
-            <div>Home</div>
-            <h1>{globalVal}</h1>
-            
-            <button onClick={handleClick}>UP</button>
-            <ul>
-                <li><mark>useContext</mark></li>
-                <li><mark>useEffect</mark></li>
-                <li><mark>useState</mark></li>
-                <li><mark>useReducer</mark></li>
-                <li><mark>useRef</mark></li>
-                {/* (값 최적화) */}
-                <li><mark>useMemo</mark></li>
-                {/* (함수 최적화) */}
-                <li><mark>useCallback</mark></li>
-            </ul>
-        </>
-    );
+  const handleClick = () => {
+    setGlobalVal(globalVal + 1);
+  };
+  return (
+    <>
+      <div>Home</div>
+      <h1>{globalVal}</h1>
+
+      <button onClick={handleClick}>UP</button>
+      <ul>
+        <li>
+          <mark>useContext</mark>
+        </li>
+        <li>
+          <mark>useEffect</mark>
+        </li>
+        <li>
+          <mark>useState</mark>
+        </li>
+        <li>
+          <mark>useReducer</mark>
+        </li>
+        <li>
+          <mark>useRef</mark>
+        </li>
+        {/* (값 최적화) */}
+        <li>
+          <mark>useMemo</mark>
+        </li>
+        {/* (함수 최적화) */}
+        <li>
+          <mark>useCallback</mark>
+        </li>
+      </ul>
+    </>
+  );
 };
 export default Home;
